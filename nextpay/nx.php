e9772d22128c15cb442eced99095db3e0703b295
Baltazar <baltazar22@protonmail.com>
2018-04-17 12:29:27 +0430

    IRR Support

--- nextpay/nx.php
+++ nextpay/nx.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * @package    Nextpay payment module
- * @author     Nextpay
- * @copyright  2014
- * @version    1.00
- */
+* @package    Nextpay payment module
+* @author     Nextpay
+* @copyright  2014
+* @version    1.00
+*/
 @session_start();
 if (isset($_GET['do'])) {
 	include (dirname(__FILE__) . '/../../config/config.inc.php');
@@ -12,36 +12,38 @@ if (isset($_GET['do'])) {
 	include_once (dirname(__FILE__).'/nextpay.php');
 	include_once (dirname(__FILE__).'/include/nextpay_payment.php');
 	$nextpay = new nextpay;
-//	if (!$cookie -> isLogged())
-//		Tools::redirect('authentication.php?back=order.php');
+	//	if (!$cookie -> isLogged())
+	//		Tools::redirect('authentication.php?back=order.php');
 	if ($_GET['do'] == 'payment') {
 		//if (isset($_POST['id'])) {
-			$nextpay -> do_payment($cart);
-	//	} else {
+		$nextpay -> do_payment($cart);
+		//	} else {
 		//	echo $nextpay -> error($nextpay -> l('There is a problem.'));
 		//}
 	} else {
 		if (isset($_POST['order_id']) && isset($_POST['trans_id']) && isset($_GET['amount'])) {
-            error_reporting(E_ALL);
+			error_reporting(E_ALL);
 			$amount = $_GET['amount'];
 			$orderId = $_POST['order_id'];
 			$trans_id = $_POST['trans_id'] ;
+			$is_rial = $_GET['IRR'] ;
 			if (isset($_SESSION['order' . $orderId])) {
 				$hash = Configuration::get('nextpay_HASH');
+
 				$hash = md5($orderId . $amount . $hash);
 				if ($hash == $_SESSION['order' . $orderId]) {
 					$api_key = Configuration::get('nextpay_API');
 
-				    $parameters = array
-                    (
-                        'api_key'	=> $api_key,
-                        'order_id'	=> $orderId ,
-                        'trans_id' 	=> $trans_id,
-                        'amount'	=> $amount
-                    );
+					$parameters = array
+					(
+						'api_key'	=> $api_key,
+						'order_id'	=> $orderId ,
+						'trans_id' 	=> $trans_id,
+						'amount'	=> $is_rial ? $amount / 10 : $amount
+					);
 
-                    $nextpay_payment = new Nextpay_Payment();
-                    $result = $nextpay_payment->verify_request($parameters);
+					$nextpay_payment = new Nextpay_Payment();
+					$result = $nextpay_payment->verify_request($parameters);
 
 					if (intval($result) == 0) {
 						$nextpay -> validateOrder($orderId, _PS_OS_PAYMENT_, $amount, $nextpay -> displayName, "سفارش تایید شده / کد تراکنش {$trans_id}", array(), $cookie -> id_currency);
